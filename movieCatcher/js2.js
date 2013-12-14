1ad2e77837f12364969695384ce46915d4f68c14
Twbadkid <kkkoooiii2@yahoo.com.tw>
2013-12-15 03:11:17 +0800

    resend

---
+++ movieCatcher/js2.js
@@ -0,0 +1,12 @@
+var block;
+
+window.onload=function(){
+    var width = Math.max(960, innerWidth),
+    height = 460;
+
+ 
+
+ block = append("block")
+    .attr("width", width)
+    .attr("height", height);    
+}
